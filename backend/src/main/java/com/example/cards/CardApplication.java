1b9b9bb390d2b5b5febf3e2cd038d3b889fcbd47
andreaflo <71915902+andreaflo@users.noreply.github.com>
2026-05-29 20:41:37 +0200

    Add whitespace for code formatting in CardApplication

--- backend/src/main/java/com/example/cards/CardApplication.java
+++ backend/src/main/java/com/example/cards/CardApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CardApplication {
  public static void main(String[] args) {
+  
   SpringApplication.run(CardApplication.class, args);
+ 
  }
 }
